3e7214621c53ac0c5f6baf41cfceb71b21ed0c33
Pietroo03 <pietro.ponte.03@gmail.com>
2025-02-18 15:21:05 +0100

    create class Prodotto and Main class

---
+++ src/org/exercise/java/shop/Main.java
@@ -0,0 +1,8 @@
+package org.exercise.java.shop;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/exercise/java/shop/Prodotto.java
@@ -0,0 +1,74 @@
+package org.exercise.java.shop;
+
+import java.util.Random;
+
+public class Prodotto {
+
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private float prezzo;
+    private static int iva = 22;
+
+    public Prodotto(String nome, String descrizione, float prezzo) {
+        Random rand = new Random();
+        this.codice = rand.nextInt(999999);
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+    }
+
+    public Prodotto() {
+        Random rand = new Random();
+        this.codice = rand.nextInt(999999);
+        this.nome = "";
+        this.descrizione = "";
+        this.prezzo = 0.0f;
+    }
+
+    public int getCodice() {
+        return this.codice;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return this.descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public float getPrezzoBase() {
+        return this.prezzo;
+    }
+
+    public void setPrezzoBase(float prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public static int getIva() {
+        return iva;
+    }
+
+    public static void setIva(int nuovaIva) {
+        iva = nuovaIva;
+    }
+
+    public float getPrezzoConIva() {
+        float prezzoConIva = prezzo + (prezzo / 100 * iva);
+        return Math.round(prezzoConIva * 100) / 100.0f;
+    }
+
+    public String getNomeEsteso() {
+        String nomeEsteso = Integer.toString(codice) + " - " + nome;
+        return nomeEsteso;
+    }
+}

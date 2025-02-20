6d39ea84fe12852afd738d96c36351922488ba30
Pietroo03 <pietro.ponte.03@gmail.com>
2025-02-20 11:09:09 +0100

    add changes

--- src/org/exercise/java/shop/Carrello.java
+++ src/org/exercise/java/shop/Carrello.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Carrello {
 
     protected Prodotto[] carrelloProdotti;
-    Smartphone onePlus = new Smartphone("OnePlus Nord 2", "Telefono ultra potente", 453f, "115 Gb");
-    Cuffie logitech = new Cuffie("Logitech G432", "Cuffie con riduzione del rumore", 125f, "nere e blu", false);
-    Televisore samsung = new Televisore("Samsung ultra potente 2k25", "Alta qualità sempre!", 875f, "32 pollici", true);
-
-    protected String prodotto;
-    Scanner in = new Scanner(System.in);
+    protected Smartphone onePlus = new Smartphone("OnePlus Nord 2", "Telefono ultra potente", 453f, "115 Gb");
+    protected Cuffie logitech = new Cuffie("Logitech G432", "Cuffie con riduzione del rumore", 125f, "nere e blu", false);
+    protected Televisore samsung = new Televisore("Samsung ultra potente 2k25", "Alta qualità sempre!", 875f, "32 pollici", true);
 
     public Carrello() {
         carrelloProdotti = new Prodotto[0];
     }
 
     public void aggiungiProdotto() {
+        Scanner in = new Scanner(System.in);
+        String inputInserito;
+
         Prodotto[] nuovoCarrelloProdotti = new Prodotto[carrelloProdotti.length + 1];
 
         for (int i = 0; i < carrelloProdotti.length; i++) {
@@ -26,20 +26,47 @@ public class Carrello {
         carrelloProdotti = nuovoCarrelloProdotti;
 
         System.out.print("Inserisci il nome del tuo prodotto: ");
-        prodotto = in.nextLine();
+        inputInserito = in.nextLine();
 
-        if (prodotto.strip().toLowerCase().equals("oneplus".strip().toLowerCase())) {
-            nuovoCarrelloProdotti[nuovoCarrelloProdotti.length - 1] = onePlus;
+        // cambaire con switch, chiedere il tipo di prodotto, non il titolo
+
+        switch (inputInserito.toLowerCase().strip()) {
+            case "oneplus":
+                carrelloProdotti[carrelloProdotti.length - 1] = onePlus;
+                System.out.println("Prodotto aggiunto alla tua lista");
+            break;
+
+            case "logitech":
+            carrelloProdotti[carrelloProdotti.length - 1] = logitech;
             System.out.println("Prodotto aggiunto alla tua lista");
-        } else if (prodotto.strip().toLowerCase().equals("logitech".strip().toLowerCase())) {
-            nuovoCarrelloProdotti[nuovoCarrelloProdotti.length - 1] = logitech;
-        } else if (prodotto.strip().toLowerCase().equals("samsung".strip().toLowerCase())) {
-            nuovoCarrelloProdotti[nuovoCarrelloProdotti.length - 1] = samsung;
-        } else {
-            System.out.println("Nessun prodotto corrisponde a: " + prodotto);
+            break;
+
+            case "samsung":
+            carrelloProdotti[carrelloProdotti.length - 1] = samsung;
+            System.out.println("Prodotto aggiunto alla tua lista");
+            break;
+        
+            default:
+                System.out.println("Nessun prodotto corrisponde a: " + inputInserito);
+                break;
         }
 
+
+
+
+        /* if (inputInserito.strip().toLowerCase().equals("oneplus".strip().toLowerCase())) {
+            carrelloProdotti[carrelloProdotti.length -1] = onePlus;
+            System.out.println("Prodotto aggiunto alla tua lista");
+        } else if (inputInserito.strip().toLowerCase().equals("logitech".strip().toLowerCase())) {
+            carrelloProdotti[carrelloProdotti.length - 1] = logitech;
+        } else if (inputInserito.strip().toLowerCase().equals("samsung".strip().toLowerCase())) {
+            carrelloProdotti[carrelloProdotti.length - 1] = samsung;
+        } else {
+            System.out.println("Nessun prodotto corrisponde a: " + inputInserito);
+        } */
+
         in.close();
+
     }
 
     public String getCarrello() {
@@ -57,12 +84,11 @@ public class Carrello {
 
     public static void main(String[] args) {
 
-
         Carrello carrello = new Carrello();
 
         carrello.aggiungiProdotto();
-        carrello.aggiungiProdotto();
-
+        
         System.out.println(carrello.getCarrello());
+
     }
 }
